df0e39ea63a8cf321f48c8d64311cd62cbdaa7cc
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-09-30 01:13:16 -0400

    add better defaults for Input/fix classname bug

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,7 @@ import './Input.css';
 
 class Input extends Component {
   render() {
-    const { cls, onChange, type, placeholder, value, name } = this.props;
+    const { cls = "", onChange, type = "text", placeholder = "Type something...", value = "", name } = this.props;
     return <input className={cls + "form-input"} {...{ onChange, type, placeholder, value, name }}/>;
   }
 }
