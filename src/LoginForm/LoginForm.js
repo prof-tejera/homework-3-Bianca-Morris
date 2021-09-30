0c39d5dc67bc73a3abc91bfa9b15d106f2856bec
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-09-29 22:37:34 -0400

    add basic login form functionality

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -2,8 +2,39 @@ import { Component } from 'react';
 import './LoginForm.css';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  clearForm() {
+    this.setState({ username: "", password: "" });
+  }
+
+  onTextChange(e) {
+    const { target: { value = "", name = null } = {} } = e;
+    
+    if (!name) { throw new Error("Cannot find name of input"); }
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    const { username } = this.state;
+    alert(`Welcome, ${username}!`);
+    e.preventDefault();
+  }
+
   render() {
-    return null;
+    return (
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+        <input type="text" name="username" onChange={(e) => this.onTextChange(e)}/>
+        <input type="password" name="password" onChange={(e) => this.onTextChange(e)}/>
+        <input type="submit" value="Login" />
+      </form>
+    );
   }
 }
 
